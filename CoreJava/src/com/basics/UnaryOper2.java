3f4411a91cff22e5ac8ddea7baadf80ebbac984a
RakeshNadendla <RakeshNadendla@Rakhee>
2018-04-10 12:08:17 -0500

    added Unaryop2

--- CoreJava/src/com/basics/UnaryOper2.java
+++ CoreJava/src/com/basics/UnaryOper2.java
@@ -3,8 +3,12 @@ package com.basics;
 public class UnaryOper2 {
 
 	public static void main(String[] args) {
-		
-
+		int x = 1, y = 5;
+		boolean z=true;
+		System.out.println(+(x + y));
+		System.out.println(-(x + y));
+		System.out.println(~(x + y));
+		System.out.println(!z);
 	}
 
 }
